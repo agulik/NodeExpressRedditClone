6f9197342e3e2cc2eaa47573185a4a4a55695d4a
Andrew Gulik <andrewgulik@gmail.com>
2017-04-21 21:08:27 +0000

    fixed margin issues

--- views/mixins/Testing2.js
+++
@@ -1,8 +0,0 @@
-app.get('/createPost', onlyLoggedIn, function(request, response) {
-    myReddit.getAllSubreddits()
-        .then(results => {
-            response.render('create-post-form', {
-                subredditOptions: results
-            });
-        });
-});
